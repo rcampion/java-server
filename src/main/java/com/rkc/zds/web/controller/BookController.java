c69eb9964759306f8d7414819a041f91ce63330f
Richard Campion <richard.campion@zdslogic.com>
2022-10-17 12:58:34 -0400

    Update

--- src/main/java/com/rkc/zds/web/controller/BookController.java
+++ src/main/java/com/rkc/zds/web/controller/BookController.java
@@ -46,31 +46,7 @@ public class BookController {
 
 		@Override
 		public void handle(HttpExchange httpExchange) throws IOException {
-
-			URI uri = httpExchange.getRequestURI();
-			String test = "Hello From handleRequest: " + uri;
-
-			// URI uri = new URI("http://example.com/foo/bar/42?param=true");
-			String[] segments = uri.getPath().split("/");
-			String lastSegment = segments[segments.length - 1];
-
-			if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
-
-				httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers",
-						"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin, apiKey");
-
-				httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", "http://localhost:4200");
-
-				httpExchange.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
-
-				httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
-
-				httpExchange.sendResponseHeaders(204, -1);
-
-				return;
-
-			}
-
+					
 			Headers r = httpExchange.getResponseHeaders();
 			r.clear();
 
@@ -80,163 +56,69 @@ public class BookController {
 
 			httpExchange.getResponseHeaders().add("Content-type", "application/json");
 
+			if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+				
+				handleOptions(httpExchange);
+
+			}
+
 			if (httpExchange.getRequestMethod().equalsIgnoreCase("GET")) {
-
-				if (!lastSegment.equalsIgnoreCase("books")) {
-
-					int id = Integer.parseInt(lastSegment);
-
-					BookDto book = bookService.findOne(id);
-					ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-					String json = null;
-					try {
-						json = ow.writeValueAsString(book);
-					} catch (JsonProcessingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					JavaServerApp.writeResponse(httpExchange, json.toString());
-
-				} else {
-
-					Map<String, String> params = SystemUtils.queryToMap(httpExchange.getRequestURI().getQuery());
-
-					System.out.println(params);
-
-					String searchParam = params.get("search");
-					String sizeParam = params.get("size");
-					String sortParam = params.get("sort");
-					String pageParam = params.get("page");
-
-					int page = Integer.parseInt(pageParam);
-					int itemsPerPage = Integer.parseInt(sizeParam);
-
-					int offset = (page) * itemsPerPage;
-
-					Pagination pagination = new Pagination(itemsPerPage, offset);
-
-					PaginationPage<BookDto> paginationPage = bookService.findBooks(pagination);
-
-					ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-					String json = null;
-					try {
-						json = ow.writeValueAsString(paginationPage);
-					} catch (JsonProcessingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					JavaServerApp.writeResponse(httpExchange, json.toString());
-
-				}
+				
+				handleGet(httpExchange);
 
 			} else if (httpExchange.getRequestMethod().equalsIgnoreCase("PUT")) {
-				try {
-
-					// REQUEST Headers
-					Headers requestHeaders = httpExchange.getRequestHeaders();
-					Set<Map.Entry<String, List<String>>> entries = requestHeaders.entrySet();
-
-					int contentLength = Integer.parseInt(requestHeaders.getFirst("Content-length"));
-
-					// REQUEST Body
-					InputStream is = httpExchange.getRequestBody();
-
-					byte[] data = new byte[contentLength];
-					int length = is.read(data);
-
-					ObjectMapper mapper = new ObjectMapper();
-
-					BookDto book = new BookDto();
-					try {
-						book = mapper.readValue(data, BookDto.class);
-					} catch (JsonParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (JsonMappingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					bookService.updateBook(book);
-
-					ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-					String json = null;
-					try {
-						json = ow.writeValueAsString(book);
-					} catch (JsonProcessingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					JavaServerApp.writeResponse(httpExchange, json.toString());
-
-					httpExchange.close();
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				
+				handlePut(httpExchange);
+				
 			} else if (httpExchange.getRequestMethod().equalsIgnoreCase("POST")) {
 
-				try {
-
-					// REQUEST Headers
-					Headers requestHeaders = httpExchange.getRequestHeaders();
-					Set<Map.Entry<String, List<String>>> entries = requestHeaders.entrySet();
-
-					int contentLength = Integer.parseInt(requestHeaders.getFirst("Content-length"));
-
-					// REQUEST Body
-					InputStream is = httpExchange.getRequestBody();
-
-					byte[] data = new byte[contentLength];
-					int length = is.read(data);
-
-					ObjectMapper mapper = new ObjectMapper();
-
-					BookDto book = new BookDto();
-					try {
-						book = mapper.readValue(data, BookDto.class);
-					} catch (JsonParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (JsonMappingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					bookService.saveBook(book);
-
-					ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-					String json = null;
-					try {
-						json = ow.writeValueAsString(book);
-					} catch (JsonProcessingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					JavaServerApp.writeResponse(httpExchange, json.toString());
-
-					httpExchange.close();
-
-					httpExchange.close();
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				handlePost(httpExchange);
+				
 			} else if (httpExchange.getRequestMethod().equalsIgnoreCase("DELETE")) {
 				
-				int bookId = Integer.parseInt(lastSegment);
+				handleDelete(httpExchange);
 				
-				BookDto book = bookService.findOne(bookId);
+			}
+		}
+		
+		/**
+		 * 
+		 * @param httpExchange
+		 * @throws IOException
+		 */
+		private void handleOptions(HttpExchange httpExchange) throws IOException{
+			httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers",
+					"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin, apiKey");
+
+			httpExchange.getResponseHeaders().set("Access-Control-Allow-Origin", "http://localhost:4200");
+
+			httpExchange.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
+
+			httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
+
+			httpExchange.sendResponseHeaders(204, -1);
+
+			return;
+			
+		}
+		
+		/**
+		 * 
+		 * @param httpExchange
+		 * @throws IOException
+		 */
+		private void handleGet(HttpExchange httpExchange) throws IOException {
+			
+			URI uri = httpExchange.getRequestURI();
+
+			String[] segments = uri.getPath().split("/");
+			String lastSegment = segments[segments.length - 1];
+			
+			if (!lastSegment.equalsIgnoreCase("books")) {
+
+				int id = Integer.parseInt(lastSegment);
+
+				BookDto book = bookService.findOne(id);
 				ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 				String json = null;
 				try {
@@ -245,12 +127,187 @@ public class BookController {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				bookService.deleteBook(bookId);	
 
 				JavaServerApp.writeResponse(httpExchange, json.toString());
+
+			} else {
+
+				Map<String, String> params = SystemUtils.queryToMap(httpExchange.getRequestURI().getQuery());
+
+				System.out.println(params);
+
+				String searchParam = params.get("search");
+				String sizeParam = params.get("size");
+				String sortParam = params.get("sort");
+				String pageParam = params.get("page");
+
+				int page = Integer.parseInt(pageParam);
+				int itemsPerPage = Integer.parseInt(sizeParam);
+
+				int offset = (page) * itemsPerPage;
+
+				Pagination pagination = new Pagination(itemsPerPage, offset);
+
+				PaginationPage<BookDto> paginationPage = bookService.findBooks(pagination);
+
+				ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+				String json = null;
+				try {
+					json = ow.writeValueAsString(paginationPage);
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				JavaServerApp.writeResponse(httpExchange, json.toString());
+
+			}			
+		}
+		
+		/**
+		 * 
+		 * @param httpExchange
+		 */
+		private void handlePost(HttpExchange httpExchange) {
 			
+			try {
+
+				// REQUEST Headers
+				Headers requestHeaders = httpExchange.getRequestHeaders();
+				Set<Map.Entry<String, List<String>>> entries = requestHeaders.entrySet();
+
+				int contentLength = Integer.parseInt(requestHeaders.getFirst("Content-length"));
+
+				// REQUEST Body
+				InputStream is = httpExchange.getRequestBody();
+
+				byte[] data = new byte[contentLength];
+				int length = is.read(data);
+
+				ObjectMapper mapper = new ObjectMapper();
+
+				BookDto book = new BookDto();
+				try {
+					book = mapper.readValue(data, BookDto.class);
+				} catch (JsonParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JsonMappingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				bookService.saveBook(book);
+
+				ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+				String json = null;
+				try {
+					json = ow.writeValueAsString(book);
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				JavaServerApp.writeResponse(httpExchange, json.toString());
+
+				httpExchange.close();
+
+				httpExchange.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			
+		}
+
+		/**
+		 * 
+		 * @param httpExchange
+		 */
+		private void handlePut(HttpExchange httpExchange) {
+			
+			try {
+
+				// REQUEST Headers
+				Headers requestHeaders = httpExchange.getRequestHeaders();
+				Set<Map.Entry<String, List<String>>> entries = requestHeaders.entrySet();
+
+				int contentLength = Integer.parseInt(requestHeaders.getFirst("Content-length"));
+
+				// REQUEST Body
+				InputStream is = httpExchange.getRequestBody();
+
+				byte[] data = new byte[contentLength];
+				int length = is.read(data);
+
+				ObjectMapper mapper = new ObjectMapper();
+
+				BookDto book = new BookDto();
+				try {
+					book = mapper.readValue(data, BookDto.class);
+				} catch (JsonParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JsonMappingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				bookService.updateBook(book);
+
+				ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+				String json = null;
+				try {
+					json = ow.writeValueAsString(book);
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				JavaServerApp.writeResponse(httpExchange, json.toString());
+
+				httpExchange.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+				
+		}
+		
+		/**
+		 * 
+		 * @param httpExchange
+		 * @throws IOException
+		 */
+		private void handleDelete(HttpExchange httpExchange) throws IOException {
+			
+			URI uri = httpExchange.getRequestURI();
+
+			String[] segments = uri.getPath().split("/");
+			String lastSegment = segments[segments.length - 1];
+			
+			int bookId = Integer.parseInt(lastSegment);
+			
+			BookDto book = bookService.findOne(bookId);
+			ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+			String json = null;
+			try {
+				json = ow.writeValueAsString(book);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			bookService.deleteBook(bookId);	
+
+			JavaServerApp.writeResponse(httpExchange, json.toString());
+				
 		}
 	}
 }
